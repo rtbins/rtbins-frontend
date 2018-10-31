71abe86e326d2ffd4246714ace68e98cd30578f7
rguptabinary <rguptabinary@gmail.com>
2018-11-01 00:14:57 +0530

    :hammer:

--- src/components/main/SubHeader.js
+++ src/components/main/SubHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // import styles from './subheader.css';
+// import styles from './subheader.css';
 import Layout from '../common/Layout';
 
 const SubHeader = ({ title }) => {
